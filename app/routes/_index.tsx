0a9708669dd4cf16e3d66d6750039bbf8f97102f
Nick Chaloult <npchaloult@gmail.com>
2024-05-22 07:21:22 -0400

    Build landing page scaffolding

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,49 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Form, Link } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Homemade Retro Board" },
+    {
+      name: "description",
+      content:
+        "A homemade, barebones retro board to replace tools which are either paid, or who we have an expiring enterprise license with",
+    },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
+    <main className="h-svh flex flex-col space-y-2 justify-center items-center">
+      <div className="grid grid-cols-2 gap-2 p-4 rounded-xl bg-white">
+        <Form className="flex flex-col space-y-2">
+          <input
+            type="text"
+            placeholder="Board ID"
+            className="p-2 rounded-lg border border-stone-200 bg-stone-100"
+          />
+          <input
+            type="text"
+            placeholder="Display name"
+            className="p-2 rounded-lg border border-stone-200 bg-stone-100"
+          />
+          <button
+            type="submit"
+            className="px-4 py-2 rounded-lg bg-purple-900 text-white hover:bg-purple-800 focus:bg-purple-800 transition"
           >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+            Join Board
+          </button>
+        </Form>
+        <Link
+          to="boards/new"
+          className="flex justify-center items-center px-4 py-2 rounded-lg bg-purple-700 text-white hover:bg-purple-600 focus:bg-purple-600 transition"
+        >
+          Create New Board
+        </Link>
+      </div>
+      <Link to="boards" className="text-sm text-stone-400 hover:underline">
+        Browse previous boards
+      </Link>
+    </main>
   );
 }
