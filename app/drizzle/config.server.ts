f261a438ff2f185546e3015fa11f8d5af7314b31
Nick Chaloult <npchaloult@gmail.com>
2024-05-23 15:22:04 -0400

    Inform Drizzle of the DB schema

--- app/drizzle/config.server.ts
+++ app/drizzle/config.server.ts
@@ -1,7 +1,8 @@
 import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
+import * as schema from "~/drizzle/schema.server";
 
 const dbPath = process.env.DATABASE_PATH || "sqlite.db";
 
 const sqlite = new Database(dbPath);
-export const db = drizzle(sqlite);
+export const db = drizzle(sqlite, { schema });
