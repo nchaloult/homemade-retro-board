fbbfc15d6177f5355bc5ccc8a98b7a98d51f3c2d
Nick Chaloult <npchaloult@gmail.com>
2024-05-23 15:45:55 -0400

    Add board page

--- app/models/boards.server.ts
+++ app/models/boards.server.ts
@@ -1,6 +1,10 @@
-import { isNull } from "drizzle-orm";
+import { asc, eq, isNull } from "drizzle-orm";
 import { db } from "~/drizzle/config.server";
-import { boards } from "~/drizzle/schema.server";
+import { boards, columns, entries } from "~/drizzle/schema.server";
+
+export async function createBoard(displayName: string) {
+  return await db.insert(boards).values({ externalId: "testing", displayName });
+}
 
 export async function getAllBoards() {
   return await db
@@ -14,6 +18,33 @@ export async function getAllBoards() {
     .where(isNull(boards.deletedAt));
 }
 
-export async function createBoard(displayName: string) {
-  return await db.insert(boards).values({ externalId: "testing", displayName });
+export async function getBoard(externalId: string) {
+  // TODO: Look for ways to reduce the number of queries we need to make?
+
+  const boardsArray = await db
+    .select({ id: boards.id, displayName: boards.displayName })
+    .from(boards)
+    .where(eq(boards.externalId, externalId))
+    .limit(1);
+  if (boardsArray.length === 0) {
+    throw new Response("Board not found", { status: 404 });
+  }
+  // Will always only contain 1 row because of the `LIMIT 1` clause attached to
+  // the query above.
+  const { id, displayName } = boardsArray[0];
+
+  const entriesArray = await db
+    .select({
+      id: entries.id,
+      content: entries.content,
+      order: entries.order,
+      upvotes: entries.upvotes,
+    })
+    .from(entries)
+    .leftJoin(columns, eq(entries.columnId, columns.id))
+    .where(eq(entries.boardId, id))
+    .groupBy(entries.columnId)
+    .orderBy(asc(columns.order), asc(entries.order));
+
+  return { displayName, entries: entriesArray };
 }
