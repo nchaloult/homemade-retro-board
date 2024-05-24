21c53a8ea700a11f74f61ee912ccd1c623b227d4
Nick Chaloult <npchaloult@gmail.com>
2024-05-24 11:24:07 -0400

    Add up arrow and plus icons

---
+++ app/icons.tsx
@@ -0,0 +1,43 @@
+export function UpArrowIcon() {
+  return (
+    <svg
+      className="w-[12px] h-[16px]"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="20"
+      fill="none"
+      viewBox="0 0 18 20"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+        d="M9 2v16m0-16 6 6m-6-6-6 6"
+      />
+    </svg>
+  );
+}
+
+export function PlusIcon() {
+  return (
+    <svg
+      className="w-[16px] h-[16px]"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+        d="M5 12h14m-7 7V5"
+      />
+    </svg>
+  );
+}
