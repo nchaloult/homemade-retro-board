c5060a84099140e52ae7570a9a40370f92ddd3bf
Nick Chaloult <npchaloult@gmail.com>
2024-05-23 22:41:05 -0400

    Add seed SQL script

---
+++ seed.sql
@@ -0,0 +1,52 @@
+INSERT INTO boards (external_id, name) VALUES ("d4tLgQ89hG", "Foo Bar");
+
+INSERT INTO columns (name, `order`, board_id) VALUES (
+    "What went well",
+    0.0,
+    1
+);
+INSERT INTO columns (name, `order`, board_id) VALUES (
+    "What didn't go well",
+    1.0,
+    1
+);
+INSERT INTO columns (name, `order`, board_id) VALUES (
+    "What should we change",
+    2.0,
+    1
+);
+INSERT INTO columns (name, `order`, board_id) VALUES (
+    "Kudos",
+    3.0,
+    1
+);
+
+INSERT INTO entries (content, author_display_name, `order`, board_id, column_id) VALUES (
+    "Lorem ipsum dolor sit amet",
+    "anonymoose",
+    0.0,
+    1,
+    1
+);
+INSERT INTO entries (content, author_display_name, `order`, board_id, column_id) VALUES (
+    "The quick brown fox jumps over the lazy dog",
+    "anonymoose",
+    1.0,
+    1,
+    1
+);
+INSERT INTO entries (content, author_display_name, `order`, board_id, column_id) VALUES (
+    "The cow jumped over the moon",
+    "Nick",
+    2.0,
+    1,
+    1
+);
+
+INSERT INTO entries (content, author_display_name, `order`, board_id, column_id) VALUES (
+    "And one more piece of feedback from Anonymoose",
+    "anonymoose",
+    0.0,
+    1,
+    3
+);
