6e1c20328c5be53e5706eff983f5635a07097c91
Nick Chaloult <npchaloult@gmail.com>
2024-05-24 11:24:19 -0400

    Add board view scaffolding

--- app/routes/board.tsx
+++ app/routes/board.tsx
@@ -1,5 +1,7 @@
 import { LoaderFunctionArgs, json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import { PlusIcon, UpArrowIcon } from "~/icons";
+import type { Entry } from "~/queries.server";
 import { getBoard } from "~/queries.server";
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -21,9 +23,77 @@ export default function Board() {
       <header className="mb-8">
         <h1 className="font-bold text-4xl">{name}</h1>
       </header>
-      <main>
-        <p>{JSON.stringify(entries)}</p>
+      <main className="flex space-x-4">
+        {entries.map((column) => (
+          <Column
+            key={column.columnId}
+            name={column.columnName}
+            entries={column.entries}
+          />
+        ))}
       </main>
     </div>
+  );
+}
+
+interface ColumnProps {
+  name: string;
+  entries: Entry[];
+}
+function Column({ name, entries }: ColumnProps) {
+  return (
+    <section className="w-80">
+      <h2 className="font-semibold text-xl mb-4">{name}</h2>
+      <div className="flex flex-col space-y-2">
+        {entries.map((entry) => (
+          <Entry
+            key={entry.id}
+            content={entry.content}
+            authorDisplayName={entry.authorDisplayName}
+            upvotes={entry.upvotes}
+          />
+        ))}
+        <NewCardButton />
+      </div>
+    </section>
+  );
+}
+
+interface EntryProps {
+  content: string;
+  authorDisplayName: string;
+  upvotes: number;
+}
+function Entry({ content, authorDisplayName, upvotes }: EntryProps) {
+  return (
+    <div
+      className={
+        "overflow-hidden rounded-xl bg-white border-2 border-stone-200 outline-none transition"
+      }
+    >
+      <div className="px-3 py-2">
+        <p>{content}</p>
+      </div>
+      <hr className="h-0.5 bg-stone-200 border-0 rounded" />
+      <div className="flex justify-between items-center px-3 py-1 bg-stone-100">
+        <span className="text-stone-400">{authorDisplayName}</span>
+        <button className="flex items-center space-x-1 px-2 py-1 -mr-2 rounded-full text-stone-400 outline-none hover:bg-stone-200 focus:bg-stone-200">
+          <UpArrowIcon />
+          <span className="text-stone-400">{upvotes}</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function NewCardButton() {
+  return (
+    <button
+      type="button"
+      className="flex justify-center items-center space-x-1 px-4 py-2 rounded-lg bg-stone-200 text-stone-900 font-semibold border-2 border-stone-300 shadow-[rgb(214_211_209)_0_4px] outline-none hover:bg-stone-100 hover:shadow-[rgb(214_211_209)_0_8px] hover:-translate-y-1 focus:bg-stone-100 focus:shadow-[rgb(214_211_209)_0_8px] focus:-translate-y-1 active:shadow-[rgb(214_211_209)_0_4px] active:translate-y-0 transition"
+    >
+      <PlusIcon />
+      <span>New Card</span>
+    </button>
   );
 }
