b0c3f0dcfd1b8aa873e67b3f12f34ec73865cc72
Nick Chaloult <npchaloult@gmail.com>
2024-05-26 20:25:01 -0400

    Make board pages scrollable from anywhere above the fold

--- app/root.tsx
+++ app/root.tsx
@@ -22,7 +22,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body className="bg-stone-100 text-stone-900">
+      <body className="h-svh flex flex-col bg-stone-100 text-stone-900">
         {children}
         <ScrollRestoration />
         <Scripts />
