db32637277365297c551cffe791906ef7a55e9c8
Nick Chaloult <npchaloult@gmail.com>
2024-05-29 16:30:02 -0400

    Add sort icon

--- app/icons.tsx
+++ app/icons.tsx
@@ -41,3 +41,25 @@ export function PlusIcon() {
     </svg>
   );
 }
+
+export function SortIcon() {
+  return (
+    <svg
+      className="w-[16px] h-[16px]"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+        d="M8 20V10m0 10-3-3m3 3 3-3m5-13v10m0-10 3 3m-3-3-3 3"
+      />
+    </svg>
+  );
+}
