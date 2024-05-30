8ebbb9154ab897a735ca59e0d279829708389e4e
Nick Chaloult <npchaloult@gmail.com>
2024-05-29 23:20:18 -0400

    Add clipboard icons

--- app/icons.tsx
+++ app/icons.tsx
@@ -63,3 +63,33 @@ export function SortIcon() {
     </svg>
   );
 }
+
+export interface ClipboardIconProps {
+  isCopied: boolean;
+}
+export function ClipboardIcon({ isCopied }: ClipboardIconProps) {
+  const clipboard =
+    "M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-5-4v4h4V3h-4Z";
+  const clipboardWithCheck =
+    "M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-6 7 2 2 4-4m-5-9v4h4V3h-4Z";
+
+  return (
+    <svg
+      className="w-[16px] h-[16px]"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="3"
+        d={isCopied ? clipboardWithCheck : clipboard}
+      />
+    </svg>
+  );
+}
